a31713594b41c87cf5d3771abf47809a38acf91c
Matt Farmer <mrfarmer777@gmail.com>
2018-07-18 12:55:08 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,9 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+
+  def get_classifications_for(boat)
+    where("boat_id=#{boat.id}").pluck(:classification_id)
+  end
 end
